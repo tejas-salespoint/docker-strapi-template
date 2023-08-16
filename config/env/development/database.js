0547a6cf08e70d36d46f575a06013acbd3a7104a
tejas-salespoint <tejas.lade@salespintinc.com>
2023-08-16 22:21:30 +0530

    update database

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -2,11 +2,11 @@ module.exports =  ({ env }) => ({
 	connection: {
 		client: 'mysql',
 		connection: {
-		host: env('DATABASE_HOST', 'dai-industry.mysql.database.azure.com'),
+		host: env('DATABASE_HOST', 'strapi.mysql.database.azure.com'),
 			port: env.int('DATABASE_PORT', 3306),
 			database: env('DATABASE_NAME', 'strapi'),
-			user: env('DATABASE_USERNAME', 'salespoint'),
-			password: env('DATABASE_PASSWORD', 'tejas@1234'),
+			user: env('DATABASE_USERNAME', 'strapisalespoint'),
+			password: env('DATABASE_PASSWORD', '$Tejaslade89$'),
 			ssl: env.bool('DATABASE_SSL', false)
 		}
 	}
